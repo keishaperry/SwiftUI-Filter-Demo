cd5ed433f39fcc4a1e2167f1b6c1c10b930de5d8
Keisha Perry <me@keishaperry.com>
2020-03-03 19:43:54 -0800

    Initial Commit

---
+++ filtertest/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  filtertest
+//
+//  Created by Keisha Perry on 3/3/20.
+//  Copyright © 2020 Keisha Perry. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, World!")
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
